b4dae5ae37e8276692a12eedd51f11474a008674
shopelec1 <93613396+shopelec1@users.noreply.github.com>
2021-11-02 22:24:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '2065572942:AAF5bgFxR4phDBQXvwFI9fcQ9-F7eQs6sJM'
 const bot = new TG_Bot(TOKEN, {polling: true})
 
 bot.on('message', msg =>{
-	bot.sendMessage(msg.chat.id, "Hi, $msg.from.first_name")
+	bot.sendMessage(msg.chat.id, 'Bot says:"Hi, $msg.from.first_name"')
 })
