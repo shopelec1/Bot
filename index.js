e7cb76959822a5e2e2d810fe820ceaed0aa7c8e9
shopelec1 <93613396+shopelec1@users.noreply.github.com>
2021-11-02 22:18:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const TG_Bot = require('node-telegrm-bot-api')
+
+const TOKEN = '2065572942:AAF5bgFxR4phDBQXvwFI9fcQ9-F7eQs6sJM'
+
+const bot = new TG_Bot(TOKEN, {polling: true})
+
+bot.on('message', msg =>{
+	bot.sendMessage(msg.chat.id,)
+})
